888755b712b1ff4fd8b4f77a4919becd95947129
开飞机的舒克 <49060931+dahouzhichi@users.noreply.github.com>
2019-04-07 19:01:23 +0800

    加入了人才招聘页的js部分！

--- 七组小组项目/人才招聘页/hiring.js
+++ 七组小组项目/人才招聘页/hiring.js
@@ -1,3 +1,12 @@
 $(function(){
-  
+	$('.see').click(function(){
+		$(this).parent().parent().siblings().toggle();
+		if ($(this).parent().parent().siblings().css('display') == 'block') {
+			$(this).parent().parent().css('background','blue');
+			$(this).css('background-image','url('+'../images/see_on.png'+')');
+		}else{
+			$(this).parent().parent().css('background','white');
+			$(this).css('background-image','url('+'../images/see.png'+')');
+		}
+	})
 })
