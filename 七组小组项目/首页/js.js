cceb0b079752edf3090e7618171e9c7e9d52dd51
开飞机的舒克 <49060931+dahouzhichi@users.noreply.github.com>
2019-04-07 20:12:12 +0800

    测试修复1
    链接跳转

---
+++ 七组小组项目/首页/js.js
@@ -0,0 +1,9 @@
+$(function(){
+$("#scroll").click(function(){
+	$(this).css("color","skyblue").siblings().css("color","black");
+	// $("body").srcollTop(700);	
+})
+$(".first").click(function(){
+	$(this).css("color","skyblue").siblings().css("color","black");	
+})
+})
